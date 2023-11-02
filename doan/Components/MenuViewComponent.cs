b11ef9456b86ec3b724d1b686123df80aa8c6c02
chienu74 <chienu74@gmail.com>
2023-11-02 21:14:27 +0700

    Add project files.

---
+++ doan/Components/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using doan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace doan.Components
+{
+    [ViewComponent(Name = "MenuView")]
+    public class MenuViewComponent : ViewComponent
+    {
+        private DataContext _context;
+        public MenuViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.Menus.Where(m => (bool)m.IsActive).OrderBy(m => m.Position).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
