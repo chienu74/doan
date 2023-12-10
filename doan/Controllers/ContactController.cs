7f6b50723f64cbbfc4259775ad5c48608f0d6942
chienu74 <114214415+chienu74@users.noreply.github.com>
2023-12-10 23:51:32 +0700

    Update ContactController.cs
    hh

--- doan/Controllers/ContactController.cs
+++ doan/Controllers/ContactController.cs
@@ -13,25 +13,12 @@ namespace doan.Controllers
         }
         public IActionResult Index()
         {
-            var ctList = (from e in _context.Contacts
-                          select new SelectListItem()
-                          {
-                              Text = e.Name,
-                              Value = e.ContactId.ToString(),
-                          }).ToList();
-            ctList.Insert(0, new SelectListItem()
-            {
-                Text = "---Select---",
-                Value = "0",
-            });
-            ViewBag.ctList = ctList;
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
-        public IActionResult Index(Contact ct)
+        public IActionResult Create(Contact ct)
         {
             if(ModelState.IsValid)
             {
@@ -39,7 +26,7 @@ namespace doan.Controllers
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(ct);
+            return View();
         }
     }
 }
