a5d4e41544bfb9492f3589627acc60ef0de0a800
chienu74 <chienu74@gmail.com>
2023-12-11 21:41:12 +0700

    cccccc

---
+++ doan/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using doan.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using doan.Utilities;
+using elFinder.NetCore;
+using doan.Areas.Admin.Models;
+
+namespace doan.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile upimg)
+        {
+            if (upimg != null)
+            {
+                string filePath = "wwwroot/files/avatar";
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(upimg.FileName);
+                string uploadedFilePath = Path.Combine(filePath, fileName);
+
+                using (var fileStream = new FileStream(uploadedFilePath, FileMode.Create))
+                {
+                    upimg.CopyTo(fileStream);
+                }
+
+                // Cập nhật trường "Avatar" của đối tượng "Account"
+                var account = _context.Accounts.Find(Functions._AccountID); // Đây chỉ là ví dụ, bạn cần tìm đúng đối tượng "Account" tương ứng với người dùng đang tải lên hình ảnh
+                account.Avatar = "/files/avatar/" + fileName; // Lưu đường dẫn tương đối của hình ảnh
+
+                _context.Update(account);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            int id = Functions._AccountID;
+            var acc = _context.Accounts.Find(id);
+            if (acc == null)
+            {
+                return NotFound();
+            }
+            return View(acc);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(account);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+    }
+}
---
+++ doan/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using doan.Models;
+using doan.Models.ho;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace doan.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly DataContext _context;
+        public ContactController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact ct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Add(ct);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ct);
+        }
+
+       
+    }
+}
